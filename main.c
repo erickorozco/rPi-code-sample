f7014c07f136063d4935ebc98870bd3d268922c1
eorozcod17@gmail.com <eorozcod17@gmail.com>
2017-11-06 21:21:05 -0600

    Initial Commit

---
+++ main.c
@@ -0,0 +1,25 @@
+
+#include <wiringPi.h>
+#include <stdint.h>
+
+
+void main(void)
+{
+  int pin = 7;
+  printf("Raspberry Pi wiringPi blink test\n");
+
+  if (wiringPiSetup() == -1)
+    exit (1);
+
+  pinMode(pin, OUTPUT);
+
+  for (;;)
+  {
+    printf("LED On\n");
+    digitalWrite(pin, 1);
+    delay(250);
+    printf("LED Off\n");
+    digitalWrite(pin, 0);
+    delay(250);
+  }
+}
